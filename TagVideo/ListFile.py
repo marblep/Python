e21686ea5a8ab9af800c4b6e9c7bf3e61610759f
marblep <marblep@hotmail.com>
2012-12-04 00:45:21 +0800

    more file info

--- TagVideo/ListFile.py
+++ TagVideo/ListFile.py
@@ -1,15 +1,35 @@
+# -*- coding: utf8 -*-
 import os
+import time
 
+videoRootPath = r'D:\Marble\bt\FlashGet'
 videoTypes = ['wmv','avi','rmvb','rm','mkv','mp4','flv','mpg','3gp']
 
+class VideoInfo:
+    def __init__(self, abspath_, filename_, size_, mtime_):
+        self.abspath = abspath_
+        self.filename = filename_
+        self.size = size_
+        self.mtime = mtime_
+        self.mtime_str = time.ctime(mtime_)
+    
 def listVideoFiles(dirname, types):
     allVideos = []
-    for(dirname,dirsub,files) in os.walk(dirname):
-        for filename in files:
-            lowerfilename = filename.lower()
-            for onetype in types:
-                if lowerfilename.endswith('.'+onetype):
-                    allVideos.append(filename)
+    try:
+        for(dirname,dirsub,files) in os.walk(dirname):
+            for filename in files:
+                lowerfilename = filename.lower()
+                for onetype in types:
+                    if lowerfilename.endswith('.'+onetype):
+                        abspath = os.path.join(os.sep,dirname,filename)
+                        info = VideoInfo(abspath,filename, \
+                                         os.path.getsize(abspath)/1024/1024, \
+                                         os.path.getmtime(abspath))
+                        allVideos.append(info)
+                        break
+    except Exception,e:
+        print e
+        
     return allVideos
     
 
@@ -20,33 +40,15 @@ def listAllTypes(dirname):
             lowerfilename = filename.lower()
             thetype = os.path.splitext(lowerfilename)[1][1:]
             if thetype not in allTypes:
+                abspath = os.path.join()
                 allTypes.append(thetype)
 
     return allTypes
 
-#'''
-videos = listVideoFiles(r'D:\Marble\bt\FlashGet', videoTypes)
-print len(videos)
-#for video in videos:
-#    print video
-#'''
-'''
-types = listAllTypes(r'D:\Marble\bt\FlashGet')
-print len(types)
-for item in types:
-    print item
-'''
+if __name__ == "__main__":
+    #'''
+    videos = listVideoFiles(videoRootPath, videoTypes)
+    print len(videos), " Videos \n"
+    for i in range(109,119):
+        print videos[i].filename, "\t", videos[i].size, '\t', videos[i].mtime_str
 
-'''
-for (dirname,subshere,fileshere) in os.walk(r'C:\Intel'):
-    print '[{0}]'.format(dirname)
-    for fname in fileshere:
-        print os.path.join(dirname, fname)
-'''
-
-'''
-dirname = r'C:'
-for file in glob.glob(dirname + '/*.txt'):
-    head, tail = os.path.split(file)
-    print head, tail, '=>', ('D:\\other\\'  + tail)
-'''
