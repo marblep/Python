b299baed4d53d7c684eeef596815ed38d58fe59d
marblep <marblep@hotmail.com>
2012-12-06 00:43:16 +0800

    file api

--- TagVideo/JsonTool.py
+++ TagVideo/JsonTool.py
@@ -3,3 +3,15 @@
 #1. read/write file
 #2. python json API
 #3. merge into ListFile.py
+
+import os
+ 
+curdir = os.path.abspath('.')
+abspath = os.path.join(curdir, 'setting.txt')
+print os.path.exists(abspath)
+
+thefile = open('setting.txt', 'w')
+thefile.write('hello \n')
+thefile.write('python \n')
+thefile.close()
+
